ebfe799a4d2be0e408b0942035fd8418a254da75
Zack Plauché <zackknowspython@gmail.com>
2022-04-08 14:01:44 +0300

    Add homepage hero styling and text (not mobile yet)

--- src/api/client.js
+++ src/api/client.js
@@ -1,2 +1,55 @@
 
-export const BASE_URL = import.meta.env.BASE_URL
+var BASE_URL
+if (import.meta.env.DEV) {
+  BASE_URL = import.meta.env.BASE_URL + 'api/'
+} else {
+  BASE_URL = 'https://api.example.com'
+}
+
+function fetchClient(BASE_URL) {
+  let options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+  return {
+    get(url, params) {
+      url = BASE_URL + url
+      if (params) {
+        url += new URLSearchParams(params)
+      }
+      options.method = 'GET'
+      return fetch(url, options).then(response => response.json()).catch(error => { console.log('Error: ', error)})
+    },
+    post(url, data) {
+      url = BASE_URL + url
+      options.method = 'POST'
+      options.body = JSON.stringify(data)
+      return fetch(url, options)
+              .then(response => response.json())
+              .catch(error => { console.log('Error: ', error)})
+    }
+  }
+}
+
+
+export const client = fetchClient(BASE_URL)
+
+const models = {
+  'organization': 'organizations',
+}
+
+export function model(model) {
+  let url = models[model]
+  if (import.meta.env.DEV) {
+    url += '.json'
+  } else {
+    url += '/'
+  }
+  return {
+    get(params) {
+      return client.get(url, params)
+    }
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
-    {{ BASE_URL }}
+<div>
+
+  <section class="shadow">
+    <div class="mx-auto max-w-7xl px-2 py-10 sm:py-20">
+      <div class="text-center">
+        <div class="text-7xl">🌻</div>
+      <h1 class="text-5xl sm:text-6xl font-semibold text-neutral-800 mt-16 sm:leading-snug">
+        Trustworthy Non-Profits to <br>Help
+        <span class="bg-clip-text bg-gradient-to-r from-sky-600 to-yellow-400 text-transparent">Ukraine</span>.
+      </h1>
+      <p class="text-4xl mt-5 font-light text-neutral-700">Help Ukraine defend against Russian aggression.</p>
+      <div class="flex flex-wrap gap-5 justify-center mt-10">
+        <router-link class="btn btn-hero bg-sky-500 hover:bg-sky-600 text-white relative flex-1 sm:flex-none" to>
+          Help Ukraine Defend Itself
+          <span class="absolute inset-x-10 inset-y-2 bg-inherit rounded-full animate-ping hidden sm:inline -z-10 flex-1 sm:flex-none"></span>
+        </router-link>
+        <router-link class="btn btn-hero bg-yellow-400 text-white" to>Humanitarian Aid</router-link>
+      </div>
+    </div>
   </div>
+</section>
+
+</div>
 </template>
 
 <script setup>
-const BASE_URL = import.meta.env.BASE_URL
+import { model } from '@/api/client'
+import { onMounted, ref } from 'vue'
+
+const data = ref([])
+
+onMounted(async () => {
+  data.value = await model('organization').get()
+})
+
 </script>
+
+<style scoped>
+
+.btn {
+  @apply inline-flex justify-center items-center whitespace-nowrap
+}
+
+.btn-hero { 
+  @apply px-10 py-4 text-lg rounded-full transition-colors
+}
+</style>
